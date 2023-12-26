3b7139e9fc4a31f88f8f3ce1e28073510f02f0ab
KangPvP <82943439+KangPvP@users.noreply.github.com>
2023-12-26 22:41:28 +0100

    Video 12:47

--- flutter_app_3/lib/pages/home.dart
+++ flutter_app_3/lib/pages/home.dart
@@ -17,6 +17,7 @@ class HomePage extends StatelessWidget {
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: appBar(),
+        backgroundColor: Colors.white,
         body: Column(
           children: [
             Container(
@@ -24,7 +25,7 @@ class HomePage extends StatelessWidget {
               decoration: BoxDecoration(
                 boxShadow: [
                   BoxShadow(
-                    color: Color(0xff1D1617).withOpacity(0.11),
+                    color: const Color(0xff1D1617).withOpacity(0.11),
                     blurRadius: 40,
                     spreadRadius: 0.0,
                   )
@@ -35,10 +36,35 @@ class HomePage extends StatelessWidget {
                   filled: true, 
                   fillColor: Colors.white,
                   contentPadding: const EdgeInsets.all(15),
+                  hintText: 'Search Pancake',
+                  hintStyle: TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset('assets/icons/Search.svg')
+                    ),
+                  suffixIcon: Container( 
+                    width:100, 
+                    child: IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const VerticalDivider(
+                          color: Colors.black, 
+                          indent: 10, 
+                          endIndent: 10, 
+                          thickness: 0.1,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset('assets/icons/Filter.svg')
+                        )
+                    ])
+                  )),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(15),
                     borderSide: BorderSide.none
-                  )),
+                  )
+                ),
               ),
             ),
         ]),
