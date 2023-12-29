bdc93a6b97f34cd5732dcb30ffaea0bca9a1052e
KangPvP <82943439+KangPvP@users.noreply.github.com>
2023-12-29 17:50:22 +0100

    Your message

--- flutter_app_3/lib/pages/home.dart
+++ flutter_app_3/lib/pages/home.dart
@@ -21,33 +21,72 @@ class HomePage extends StatelessWidget {
           children: [
             _searchField(),
             SizedBox(height: 40,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: Text('Category',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18, 
-                      fontWeight: FontWeight.w600
-                  )),
-                ),
-                
-                SizedBox(height: 20),
-                Container(
-                  height: 150,
-                  color: Colors.green,
-                  child: ListView.builder(
-                    itemBuilder: (context, build) {
-                      return Container(); //container == widget
-                    },
-                  )
-                )
-              ],
-            ),
+            _categoriesSection(),
         ]),
       );
+    }
+
+    Column _categoriesSection() {
+      return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text('Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18, 
+                    fontWeight: FontWeight.w600
+                )),
+              ),
+              
+              SizedBox(height: 20),
+              Container(
+                height: 120,
+                child: ListView.separated(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(left: 20, right: 20,),
+                  separatorBuilder: (context, index) => SizedBox(width: 25,),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: categories[index].boxColor.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(16)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Padding(
+                              
+                              padding: const EdgeInsets.all(8.0),
+                              child: SvgPicture.asset(categories[index].iconPath)
+                            ),
+                          ),
+                          Text(
+                            categories[index].name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                              fontSize: 14,
+                            ),
+                          )
+                        ]
+                      ),
+                    ); //container == widget
+                  },
+                )
+              )
+            ],
+          );
     }
 
     AppBar appBar() {
